d7747db29dc852177160c52d90a60652cf4e7145
yellowkunn <ahsanlubis308@gmail.com>
2025-10-23 15:02:28 +0700

    update model

--- app/Models/Aboutus.php
+++ app/Models/Aboutus.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Aboutus extends Model
+class AboutUs extends Model
 {
     use HasFactory;
     protected $table = 'tentang_kami';
