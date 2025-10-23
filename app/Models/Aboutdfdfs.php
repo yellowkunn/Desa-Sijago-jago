f7109060baf2c750670b488ae786735fa17ccb5f
yellowkunn <ahsanlubis308@gmail.com>
2025-10-23 15:04:31 +0700

    dummy

--- app/Models/Aboutdfdfs.php
+++ app/Models/Aboutdfdfs.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AboutUs extends Model
+class Aboutdfdfs extends Model
 {
     use HasFactory;
     protected $table = 'tentang_kami';
